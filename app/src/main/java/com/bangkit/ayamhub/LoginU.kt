61f4bb892b10d9674088da3efd30e7bd4b66da95
Khair Fadhillah <80555246+khafadhil@users.noreply.github.com>
2023-05-21 10:12:54 +0700

    Create XML
    Welcome, login, import font, asset and many other

--- app/src/main/java/com/bangkit/ayamhub/LoginU.kt
+++ app/src/main/java/com/bangkit/ayamhub/LoginU.kt
@@ -3,22 +3,21 @@ package com.bangkit.ayamhub
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bangkit.ayamhub.databinding.ActivityLoginUBinding
 import com.bangkit.ayamhub.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class LoginU : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivityLoginUBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         supportActionBar?.hide()
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityLoginUBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.loginButton.setOnClickListener {
-            startActivity(Intent(this, LoginU::class.java))
-            finish()
+        binding.daftar.setOnClickListener {
+
         }
-
-
     }
 }

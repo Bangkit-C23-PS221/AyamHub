9fcc06350fac547c2397f1a05a3e8499023219e4
daffaasha <aadaffa41@gmail.com>
2023-05-23 09:17:55 +0700

    add: header for apiconfig
    adding header for authorization using token inside the ApiConfig class

--- app/src/main/java/com/bangkit/ayamhub/data/online/retrofit/ApiConfig.kt
+++ app/src/main/java/com/bangkit/ayamhub/data/online/retrofit/ApiConfig.kt
@@ -1,26 +1,35 @@
 package com.bangkit.ayamhub.data.online.retrofit
 
-import androidx.viewbinding.BuildConfig
+import okhttp3.Interceptor
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object ApiConfig {
-    fun getApiService(): ApiService {
-        val loggingInterceptor = if(BuildConfig.DEBUG) {
-            HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
-        } else {
-            HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.NONE)
+    private const val BASE_URL = "Insert Base URL"
+    fun getApiService(token: String = ""): ApiService {
+        val loggingInterceptor = HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+
+        val headerInterceptor = Interceptor {
+            var request = it.request()
+            request = request.newBuilder()
+                .addHeader("Authorization", "Bearer: $token")
+                .build()
+            it.proceed(request)
         }
+
         val client = OkHttpClient.Builder()
             .addInterceptor(loggingInterceptor)
+            .addInterceptor(headerInterceptor)
             .build()
+
         val retrofit = Retrofit.Builder()
-            .baseUrl("Insert Base URL")
+            .baseUrl(BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())
             .client(client)
             .build()
+
         return retrofit.create(ApiService::class.java)
     }
 }

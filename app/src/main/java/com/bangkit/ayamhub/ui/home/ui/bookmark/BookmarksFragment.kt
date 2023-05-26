2e1d46e2cea9c5e88c18597a17a957507cf19178
Khair Fadhillah <80555246+khafadhil@users.noreply.github.com>
2023-05-26 08:18:24 +0700

    fixing botnav
    change default icon and fragment botnav, refactor package and file, delete unused element

--- app/src/main/java/com/bangkit/ayamhub/ui/home/ui/bookmark/BookmarksFragment.kt
+++ app/src/main/java/com/bangkit/ayamhub/ui/home/ui/bookmark/BookmarksFragment.kt
@@ -1,4 +1,4 @@
-package com.bangkit.ayamhub.ui.home.ui.notifications
+package com.bangkit.ayamhub.ui.home.ui.bookmark
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -10,7 +10,7 @@ import androidx.lifecycle.ViewModelProvider
 import com.bangkit.ayamhub.databinding.FragmentNotificationsBinding
 
 
-class NotificationsFragment : Fragment() {
+class BookmarksFragment : Fragment() {
 
     private var _binding: FragmentNotificationsBinding? = null
 
@@ -24,7 +24,7 @@ class NotificationsFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         val notificationsViewModel =
-            ViewModelProvider(this).get(NotificationsViewModel::class.java)
+            ViewModelProvider(this).get(BookmarksViewModel::class.java)
 
         _binding = FragmentNotificationsBinding.inflate(inflater, container, false)
         val root: View = binding.root
--- app/src/main/java/com/bangkit/ayamhub/ui/home/ui/bookmark/BookmarksViewModel.kt
+++ app/src/main/java/com/bangkit/ayamhub/ui/home/ui/bookmark/BookmarksViewModel.kt
@@ -1,13 +1,13 @@
-package com.bangkit.ayamhub.ui.home.ui.dashboard
+package com.bangkit.ayamhub.ui.home.ui.bookmark
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class DashboardViewModel : ViewModel() {
+class BookmarksViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is dashboard Fragment"
+        value = "This is notifications Fragment"
     }
     val text: LiveData<String> = _text
 }

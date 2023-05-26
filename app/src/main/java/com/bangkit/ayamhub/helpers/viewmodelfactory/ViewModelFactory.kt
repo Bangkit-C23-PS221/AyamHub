572c4d23700939b2b1db58e05c8259dd9460b009
daffaasha <aadaffa41@gmail.com>
2023-05-26 14:55:56 +0700

    update: viewmodelfactory
    adding DetectionViewModel to the viewmodelfactory

--- app/src/main/java/com/bangkit/ayamhub/helpers/viewmodelfactory/ViewModelFactory.kt
+++ app/src/main/java/com/bangkit/ayamhub/helpers/viewmodelfactory/ViewModelFactory.kt
@@ -6,11 +6,12 @@ import androidx.lifecycle.ViewModelProvider
 import com.bangkit.ayamhub.data.repository.FarmRepository
 import com.bangkit.ayamhub.data.repository.UserRepository
 import com.bangkit.ayamhub.helpers.injection.Injection
+import com.bangkit.ayamhub.ui.home.ui.detection.DetectionViewModel
 import com.bangkit.ayamhub.ui.login.LoginViewModel
 import com.bangkit.ayamhub.ui.register.RegisterViewModel
 
 class ViewModelFactory(
-//    private val farmRepository: FarmRepository,
+    private val farmRepository: FarmRepository,
     private val userRepository: UserRepository
     ) : ViewModelProvider.NewInstanceFactory() {
 
@@ -19,6 +20,7 @@ class ViewModelFactory(
 //            modelClass.isAssignableFrom(LoginPeternakanViewModel::class.java) -> return LoginPeternakanViewModel(userRepository) as T
             modelClass.isAssignableFrom(LoginViewModel::class.java) -> return LoginViewModel(userRepository) as T
             modelClass.isAssignableFrom(RegisterViewModel::class.java) -> return RegisterViewModel(userRepository) as T
+            modelClass.isAssignableFrom(DetectionViewModel::class.java) -> return DetectionViewModel(farmRepository ,userRepository) as T
 //            modelClass.isAssignableFrom(RegisterPeternakViewModel::class.java) -> return RegisterPeternakViewModel(userRepository) as T
         }
         throw IllegalArgumentException("Unknown ViewModel Class: ${modelClass.name}")
@@ -30,7 +32,7 @@ class ViewModelFactory(
         fun getInstance(context: Context): ViewModelFactory =
             instance ?: synchronized(this) {
                 instance ?: ViewModelFactory(
-//                    Injection.provideFarmRepository(),
+                    Injection.provideFarmRepository(),
                     Injection.provideUserRepository(context))
             }.also { instance = it }
     }

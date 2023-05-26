6b01b74518ff78f3164a2320a1545e12e53bc6c7
daffaasha <aadaffa41@gmail.com>
2023-05-26 19:47:42 +0700

    add: datadummy for homepage
    adding data dummy using random data from my local storage for testing purposes

---
+++ app/src/main/java/com/bangkit/ayamhub/data/local/FlameChaser.kt
@@ -0,0 +1,10 @@
+package com.bangkit.ayamhub.data.local
+
+data class FlameChaser (
+val id: Int,
+val name: String,
+val signet: String,
+val quote: String,
+val description: String,
+val photo: String
+)

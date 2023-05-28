06de5f62caf696794f4ccb977709e5e1bf81cd06
Khair Fadhillah <80555246+khafadhil@users.noreply.github.com>
2023-05-28 22:03:06 +0700

    implementation detection for fragment detection
    wow, the implementation for image classification has done

--- app/src/main/java/com/bangkit/ayamhub/ui/homepage/ui/detection/DetectionFragment.kt
+++ app/src/main/java/com/bangkit/ayamhub/ui/homepage/ui/detection/DetectionFragment.kt
@@ -1,12 +1,9 @@
 package com.bangkit.ayamhub.ui.homepage.ui.detection
 
-
-
-import android.app.Activity.RESULT_OK
 import android.content.Intent
-import android.content.Intent.ACTION_GET_CONTENT
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
+import android.media.ThumbnailUtils
 import android.net.Uri
 import android.os.Bundle
 import android.provider.MediaStore
@@ -14,21 +11,29 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.graphics.drawable.toBitmap
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.bangkit.ayamhub.databinding.FragmentDetectionBinding
 import com.bangkit.ayamhub.helpers.uriToFile
 import com.bangkit.ayamhub.helpers.viewmodelfactory.ViewModelFactory
+import java.io.IOException
+import java.nio.ByteBuffer
+import java.nio.ByteOrder
+import org.tensorflow.lite.Interpreter
+import java.io.FileInputStream
+import java.nio.channels.FileChannel
 
-
-open class DetectionFragment : Fragment() {
-    private var imageSize = 32
+class DetectionFragment : Fragment() {
+    private var imageSize = 224
 
     private var _binding: FragmentDetectionBinding? = null
     private val binding get() = _binding!!
     private val viewModel: DetectionViewModel by viewModels {
         ViewModelFactory.getInstance(requireContext())
     }
+
+    private lateinit var interpreter: Interpreter
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -42,15 +47,35 @@ open class DetectionFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        showImage()
+        setupInterpreter()
 
+        showImage()
         binding.btnCamera.setOnClickListener { startCamera() }
         binding.btnGalery.setOnClickListener { startGallery() }
+        binding.btnClassify.setOnClickListener { classifyImage() }
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun setupInterpreter() {
+        try {
+            val options = Interpreter.Options()
+            interpreter = Interpreter(loadModelFile(), options)
+        } catch (e: IOException) {
+            // Handle the exception
+        }
+    }
+
+    private fun loadModelFile(): ByteBuffer {
+        val fileDescriptor = requireContext().assets.openFd("newmodel.tflite")
+        val inputStream = FileInputStream(fileDescriptor.fileDescriptor)
+        val fileChannel = inputStream.channel
+        val startOffset = fileDescriptor.startOffset
+        val declaredLength = fileDescriptor.declaredLength
+        return fileChannel.map(FileChannel.MapMode.READ_ONLY, startOffset, declaredLength)
     }
 
     private fun startCamera() {
@@ -65,18 +90,57 @@ open class DetectionFragment : Fragment() {
     }
 
     private fun startGallery() {
-        val intent = Intent()
-        intent.action = ACTION_GET_CONTENT
+        val intent = Intent(Intent.ACTION_GET_CONTENT)
         intent.type = "image/*"
         val chooser = Intent.createChooser(intent, "Choose a Picture")
         launcherIntentGallery.launch(chooser)
     }
 
+    private fun classifyImage() {
+        val imageBitmap = binding.pvImage.drawable.toBitmap()
+        val resizedBitmap = ThumbnailUtils.extractThumbnail(imageBitmap, imageSize, imageSize)
+
+        val inputBuffer = preprocessImage(resizedBitmap)
+        val outputBuffer = Array(1) { FloatArray(4) }
+
+        interpreter.run(inputBuffer, outputBuffer)
+
+        val maxIndex = outputBuffer[0].indices.maxByOrNull { outputBuffer[0][it] } ?: -1
+        val classes = arrayOf("coccidiosis", "healthy", "ncd", "salmonella")
+        val className = if (maxIndex != -1) classes[maxIndex] else "Unknown"
+        binding.result.text = className
+    }
+
+    private fun preprocessImage(bitmap: Bitmap): ByteBuffer {
+        val inputShape = interpreter.getInputTensor(0).shape()
+        val inputSize = inputShape[1]
+
+        val resizedBitmap = Bitmap.createScaledBitmap(bitmap, inputSize, inputSize, true)
+
+        val inputBuffer = ByteBuffer.allocateDirect(1 * inputSize * inputSize * 3 * 4)
+        inputBuffer.order(ByteOrder.nativeOrder())
+
+        val pixels = IntArray(inputSize * inputSize)
+        resizedBitmap.getPixels(pixels, 0, inputSize, 0, 0, inputSize, inputSize)
+
+        for (pixelValue in pixels) {
+            val r = (pixelValue shr 16 and 0xFF).toFloat() / 255.0f
+            val g = (pixelValue shr 8 and 0xFF).toFloat() / 255.0f
+            val b = (pixelValue and 0xFF).toFloat() / 255.0f
+
+            inputBuffer.putFloat(r)
+            inputBuffer.putFloat(g)
+            inputBuffer.putFloat(b)
+        }
+
+        return inputBuffer
+    }
+
     private val launcherIntentCamera = registerForActivityResult(
         ActivityResultContracts.StartActivityForResult()
-    ) {
-        if (it.resultCode == RESULT_OK) {
-            val imageBitmap = it.data?.extras?.get("data") as Bitmap
+    ) { result ->
+        if (result.resultCode == android.app.Activity.RESULT_OK) {
+            val imageBitmap = result.data?.extras?.get("data") as Bitmap
             viewModel.saveImage(imageBitmap)
         }
     }
@@ -84,7 +148,7 @@ open class DetectionFragment : Fragment() {
     private val launcherIntentGallery = registerForActivityResult(
         ActivityResultContracts.StartActivityForResult()
     ) { result ->
-        if (result.resultCode == RESULT_OK) {
+        if (result.resultCode == android.app.Activity.RESULT_OK) {
             val selectedImg = result.data?.data as Uri
             selectedImg.let { uri ->
                 val myFile = uriToFile(uri, requireContext())
@@ -93,5 +157,7 @@ open class DetectionFragment : Fragment() {
         }
     }
 
-
+    companion object {
+        private const val YOUR_MODEL_PATH = "your_model.tflite"
+    }
 }

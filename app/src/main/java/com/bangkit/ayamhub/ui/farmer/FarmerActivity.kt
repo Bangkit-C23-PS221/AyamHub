073ba586a00da5258b52665a213f2b81e7cdd0de
Khair Fadhillah <80555246+khafadhil@users.noreply.github.com>
2023-05-31 18:33:32 +0700

    make farmer detail dan edit farmer
    layouting

---
+++ app/src/main/java/com/bangkit/ayamhub/ui/farmer/FarmerActivity.kt
@@ -0,0 +1,12 @@
+package com.bangkit.ayamhub.ui.farmer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.bangkit.ayamhub.R
+
+class FarmerActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_farmer)
+    }
+}
